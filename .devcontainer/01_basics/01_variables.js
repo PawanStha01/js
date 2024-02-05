0691849c68eebb9df0bf34ea6a4b65e9b7fbad0b
PawanStha01 <shresthapawan0@gmail.com>
2024-02-05 12:58:15 +0545

    container configure done

---
+++ .devcontainer/01_basics/01_variables.js
@@ -0,0 +1,7 @@
+const accountId = 16532
+let accountEmail = "test@gmail.com"
+var accountPassword = "12345"
+accountCity = "lalitpur"
+// accountId = 2
+// console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity])
