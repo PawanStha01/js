2dc27b250cf7bcb961c8e5477e601c6d84705baf
PawanStha01 <shresthapawan0@gmail.com>
2024-02-06 19:27:08 +0545

    datatypes

---
+++ .devcontainer/01_basics/02_datatypes.js
@@ -0,0 +1,34 @@
+"use strict" //treat all js code as newer version
+
+
+// alert(5 + 5) // we are using nodejs, not browser
+
+
+console.log(3 
+    + 
+    4) // code readability should be high
+console.log("pawan")
+
+
+// data types 
+// string types
+let name = "pawan"
+//number
+let age = 20
+let isLoging = false // boolean
+
+
+// nuber => 2 to power 53
+//bigint user for huge number
+
+// string => " " or ' '
+//boolean =>  true/ false
+// null   => standlone value
+//undefined => 
+// symbol => unique
+
+
+
+//object
+console.log(typeof null);
+
