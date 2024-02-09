cd3a883dbaca0e68e3db96a10baa96a7d73a795d
PawanStha01 <shresthapawan0@gmail.com>
2024-02-09 09:53:41 +0545

    update

---
+++ .devcontainer/01_basics/datatypes_summary.js
@@ -0,0 +1,34 @@
+
+// primitive 
+
+//  7 types of 
+/* 
+ string, number, boolean, null, undefined, symbol, bigint
+
+*/
+const score = 100;
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let usrEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+//console.log(id === anotherId) // falsed;
+// Referemce (non primitive) 
+// array, object, function
+
+
+
+const names = ['john', 'peter', 'bob']
+ let person ={
+    name: 'john',
+    age: 30,
+}
+
+const myFunction = function(){
+    console.log('hello world');
+}
+
+console.log(typeof isLoggedIn);
