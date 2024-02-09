5ca197854b261734e132fa5f79f57573e631d27f
PawanStha01 <shresthapawan0@gmail.com>
2024-02-09 06:56:50 +0545

    update

--- .devcontainer/01_basics/03_conversionOperation.js
+++ .devcontainer/01_basics/03_conversionOperation.js
@@ -13,11 +13,22 @@
 
 
 
-let isLoggeIn = 0
+// let isLoggeIn = 0
 
-let booleanInLoggedIn = Boolean(isLoggeIn)
-console.log(booleanInLoggedIn);
+// let booleanInLoggedIn = Boolean(isLoggeIn)
+// console.log(booleanInLoggedIn);
 
 
+
+let str1 = "hello"
+let str2 = " pawan"
+
+let str3 = str1 + str2
+console.log(str3);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
 
 
