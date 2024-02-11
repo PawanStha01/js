f53bb9f63c6a8f8fc3bc055c92dd4b0a3c162f82
PawanStha01 <shresthapawan0@gmail.com>
2024-02-11 13:14:44 +0545

    new date

---
+++ .devcontainer/01_basics/06_number_and_math.js
@@ -0,0 +1,45 @@
+// // Basic arithmetic operations
+// const a = 5;
+// const b = 3;
+
+// const sum = a + b;
+// const difference = a - b;
+// const product = a * b;
+// const quotient = a / b;
+// const remainder = a % b;
+
+// console.log("Sum:", sum);
+// console.log("Difference:", difference);
+// console.log("Product:", product);
+// console.log("Quotient:", quotient);
+// console.log("Remainder:", remainder);
+
+// // Math object
+// const radius = 5;
+// const circumference = 2 * Math.PI * radius;
+// const area = Math.PI * Math.pow(radius, 2);
+
+// console.log("Circumference:", circumference);
+// console.log("Area:", area);
+
+// // Random number
+// const random = Math.random(); // generates a random number between 0 and 1
+// console.log("Random Number:", random);
+
+// const balance = new Number(100)
+// // console.log(balance)
+// // console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+// const otheNumber = 123.8987
+
+// console.log(otheNumber.toPrecision(3));
+
+
+
+
+
+
+
+
+const number = 10000000
+console.log(number.toLocaleString('en-IN'))
